2937e8ba4b421c570aa2357539a8ad13b4e8b74f
Mateus Medeiros <mateus.med144@gmail.com>
2022-02-08 21:56:57 -0300

    Modelando classe Livro

---
+++ lib/models/book.dart
@@ -0,0 +1,47 @@
+class Book {
+  int _id;
+  String _name;
+  String _author;
+  Publisher _publisher;
+  int _releaseYear;
+  int _quantity;
+  int _totalRented;
+
+  Book(
+    this._id,
+    this._author,
+    this._name,
+    this._publisher,
+    this._quantity,
+    this._releaseYear,
+    this._totalRented,
+  );
+
+  int get id {
+    return _id;
+  }
+
+  String get name {
+    return _name;
+  }
+
+  String get author {
+    return _author;
+  }
+
+  Publisher get publisher {
+    return publisher;
+  }
+
+  int get releaseYear {
+    return _releaseYear;
+  }
+
+  int get quantity {
+    return _quantity;
+  }
+
+  int get totalRented {
+    return _totalRented;
+  }
+}
