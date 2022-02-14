eff9aad1b507407387cbbf73b2477246635088fc
Mateus Medeiros <mateus.med144@gmail.com>
2022-02-14 20:25:28 -0300

    Validando campos do formulário

--- lib/components/users/user_register_form.dart
+++ lib/components/users/user_register_form.dart
@@ -1,4 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:livraria_wda/models/user.dart';
+import 'package:provider/provider.dart';
 
 class UserRegisterForm extends StatefulWidget {
   const UserRegisterForm({Key? key}) : super(key: key);
@@ -8,10 +11,10 @@ class UserRegisterForm extends StatefulWidget {
 }
 
 class _UserRegisterFormState extends State<UserRegisterForm> {
-  final nameController = TextEditingController();
-  final addressController = TextEditingController();
-  final cityController = TextEditingController();
-  final emailController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
+  ///agrupa os dados do formulário após a validação
+  final Map<String, String> _formData = {};
 
   @override
   Widget build(BuildContext context) {
@@ -22,54 +25,109 @@ class _UserRegisterFormState extends State<UserRegisterForm> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Icon(
-                Icons.person_add_alt_1,
-                size: 90,
-              ),
-              const SizedBox(height: 20),
-              //TODO: Alterar os ícones
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Nome',
-                  prefixIcon: Icon(Icons.person),
+          child: Form(
+            key: _form,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Icon(
+                  Icons.person_add_alt_1,
+                  size: 90,
                 ),
-              ),
-              TextField(
-                controller: addressController,
-                decoration: const InputDecoration(
-                  labelText: 'Endereço',
-                  prefixIcon: Icon(Icons.location_on),
+                const SizedBox(height: 20),
+                //TODO: Alterar os ícones
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Nome',
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  onSaved: (newValue) =>
+                      _formData['name'] = newValue.toString(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório.';
+                    } else if (value.length < 3) {
+                      return 'No mínimo 3 caracteres.';
+                    } else if (value.length > 30) {
+                      return 'No máximo 30 caracteres.';
+                    }
+                  },
                 ),
-              ),
-              TextField(
-                controller: cityController,
-                decoration: const InputDecoration(
-                  labelText: 'Cidade',
-                  prefixIcon: Icon(Icons.location_city_rounded),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Endereço',
+                    prefixIcon: Icon(Icons.location_on),
+                  ),
+                  onSaved: (newValue) =>
+                      _formData['address'] = newValue.toString(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório.';
+                    } else if (value.length < 5) {
+                      return 'No mínimo 5 caracteres.';
+                    } else if (value.length > 50) {
+                      return 'No máximo 50 caracteres.';
+                    }
+                  },
                 ),
-              ),
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'E-mail',
-                  prefixIcon: Icon(Icons.mail),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Cidade',
+                    prefixIcon: Icon(Icons.location_city_rounded),
+                  ),
+                  onSaved: (newValue) =>
+                      _formData['city'] = newValue.toString(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório.';
+                    } else if (value.length < 5) {
+                      return 'No mínimo 3 caracteres.';
+                    } else if (value.length > 50) {
+                      return 'No máximo 20 caracteres.';
+                    }
+                  },
                 ),
-              ),
-            ],
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'E-mail',
+                    prefixIcon: Icon(Icons.mail),
+                  ),
+                  onSaved: (newValue) =>
+                      _formData['email'] = newValue.toString(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório.';
+                    } else if (!EmailValidator.validate(value)) {
+                      return 'E-mail inválido.';
+                    } else if (value.length < 5) {
+                      return 'No mínimo 5 caracteres.';
+                    } else if (value.length > 50) {
+                      return 'No máximo 100 caracteres.';
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          debugPrint(nameController.text);
-          debugPrint(addressController.text);
-          debugPrint(cityController.text);
-          debugPrint(emailController.text);
+          final bool isValid = _form.currentState!.validate();
+          if (isValid) {
+            _form.currentState!.save();
+            // Provider.of<UserProvider>(context).registerUser(
+            //   User(
+            //     -1,
+            //     _formData['name'],
+            //     _formData['email'],
+            //     _formData['address'],
+            //     _formData['city'],
+            //   ),
+            // );
+            Navigator.of(context).pop();
+          }
         },
         backgroundColor: Colors.green,
         child: const Icon(Icons.save),
